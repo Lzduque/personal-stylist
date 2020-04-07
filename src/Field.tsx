2ff80e9b5e7987e162905a21e157f9477d8f76fa
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-07 15:12:49 -0400

    base code and fix indentation

---
+++ src/Field.tsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import { IErrors } from "./Form"
+
+/* The avaliable editors for the field */
+type Editor = "textbox" | "multilinetextbox" | "dropdown";
+
+export interface IFieldProps {
+  //  Unique field name
+  id: string;
+
+  // The label text fo the field
+  label?: string;
+
+  // The editor for the field
+  editor?: Editor;
+
+  // The dropdownitems for the field
+  options?: string[];
+
+  // The field value
+  value?: any;
+}
+
+export const Field: React.SFC<IFieldProps> = ({
+  id,
+  label,
+  editor,
+  options,
+  value
+}) => {
+  return (
+    <div className="form-group">
+      {label && <label htmlFor={id}>{label}</label>}
+
+      {editor!.toLowerCase() === "textbox" && (
+        <input
+          id={id}
+          type="text"
+          value={value}
+          onChange={
+            (e: React.FormEvent<HTMLInputElement>) =>
+              console.log(e) /* TODO: push change to form values */
+          }
+          onBlur={
+            (e: React.FormEvent<HTMLInputElement>) =>
+            console.log(e) /* TODO: validade field value */
+          }
+          className="form-control" 
+        />
+      )}
+
+      {editor!.toLowerCase() === "multilinetextbox" && (
+        <textarea
+          id={id}
+          value={value}
+          onChange={
+            (e: React.FormEvent<HTMLTextAreaElement>) =>
+              console.log(e) /* TODO: pushchange to form values */
+          }
+          onBlur={
+            (e: React.FormEvent<HTMLTextAreaElement>) =>
+              console.log /* TODO: validate field value */
+          }
+          className="form-control"
+        />
+      )}
+
+      {editor!.toLowerCase() === "dropdown" && (
+        <select
+          id={id}
+          name={id}
+          value={value}
+          onChange={
+            (e: React.FormEvent<HTMLSelectElement>) =>
+              console.log(e) /* TODO: push change to form values */
+          }
+          onBLur={
+            (e: React.FormEvent<HTMLSelectElement>) =>
+              console.log(e) /* TODO: validate field value */
+          }
+          className="form-control"
+        >
+          {options &&
+            options.map(option => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+        </select>
+      )}
+
+      {/* TODO - display validation error */}
+    </div>
+  );
+};
+
+Field.defaultProps = {
+  editor: "textbox"
+};
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,96 +1,119 @@
-import* as React from "react";
+import * as React from "react";
 
 interface IFormProps {
-    action: string;
+  /* The http path that the form will be posted to */
+  action: string;
+
+  /* A prop wich allows contect to be injected */
+  render: () => React.ReactNode;
 }
 
 export interface IValues {
-    [key: string]: any;
+  /* Key value pairs for all the field values with key being the field name */
+  [key: string]: any;
 }
 
 export interface IErrors {
-    [key:string]: string;
+  /* The validation error messages for each field (key is the field name */
+  [key: string]: string;
 }
 
 export interface IFormState {
-    values: IValues;
-
-    errors: IErrors;
-
-    submitSuccess?: boolean;
+  /* The field values */
+  values: IValues;
+  /* The field validation error messages */
+  errors: IErrors;
+  /* Whether the form has been successfully submitted */
+  submitSuccess?: boolean;
 }
 
 export class Form extends React.Component<IFormProps, IFormState> {
-    constructor(props: IFormProps) {
-        super(props);
+  constructor(props: IFormProps) {
+    super(props);
 
-        const errors: IErrors = {};
-        const values: IValues = {};
-        this.state = {
-            errors,
-            values
-        };
-    }
-
-    private haveErrors(errors: IErrors) {
-        let haveError: boolean = false;
-        Object.keys(errors).map((key: string) => {
-            if (errors[key].length > 0) {
-                haveError = true;
-            }
-        });
-        return haveError;
-    }
-
-    private handleSubmit = async (
-        e: React.FormEvent<HTMLFormElement>
-    ): Promise<void> => {
-        e.preventDefault();
-
-        if (this.validadeForm()) {
-            const submitSuccess: boolean = await this.submitForm();
-            this.setState({ submitSuccess });
-        }
+    const errors: IErrors = {};
+    const values: IValues = {};
+    this.state = {
+      errors,
+      values
     };
+  }
 
-    private validadeForm(): boolean {
-        // TODO - submit the form
-        return true;
+  /**
+   * Returns whether there are any errors in the errors object that is passed in
+   * @param {IErrors} errors - The field errors
+   */
+  private haveErrors(errors: IErrors) {
+    let haveError: boolean = false;
+    Object.keys(errors).map((key: string) => {
+      if (errors[key].length > 0) {
+        haveError = true;
+      }
+    });
+    return haveError;
+  }
+
+  /**
+   * Handles form submission
+   * @param {React.FormEvent<HTMLFormElement>} e - The form event
+   */
+  private handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    if (this.validadeForm()) {
+      const submitSuccess: boolean = await this.submitForm();
+      this.setState({ submitSuccess });
     }
+  };
 
-    private async submitForm(): Promise<boolean> {
-        // TODO - submit the form
-        return true;
-    }
+  /**
+   * Executes the validation rules for all the fields on the form and sets the error state
+   * @returns {boolean} - Whether the form is valid or not
+   */
+  private validadeForm(): boolean {
+    // TODO - submit the form
+    return true;
+  }
 
-    public render() {
-        const { submitSuccess, errors } = this.state;
-        return (
-            <form onSubmit={this.handleSubmit} noValidate={true}>
-                <div className="container">
-                    {/* {TODO - render fields} */}
-                    <div className="form-group">
-                        <button
-                            type="submit"
-                            className="btn btn-primary"
-                            disabled={this.haveErrors(errors)}
-                        >
-                            Submit
-                        </button>
-                    </div>
-                    {submitSuccess && (
-                        <div className="alert alert-info" role="alert">
-                            The form was sucessfully submitted!
-                        </div>
-                    )}
-                    {submitSuccess === false} &&
+  private async submitForm(): Promise<boolean> {
+    // TODO - submit the form
+    return true;
+  }
+
+  /**
+   * Submits the form to the http api
+   * @returns {boolean} - Whether the form submission was successful or not
+   */
+  public render() {
+    const { submitSuccess, errors } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} noValidate={true}>
+        <div className="container">
+          {/* {TODO - render fields} */}
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={this.haveErrors(errors)}
+            >
+            Submit
+            </button>
+          </div>
+          {submitSuccess && (
+            <div className="alert alert-info" role="alert">
+              The form was sucessfully submitted!
+            </div>
+          )}
+          {submitSuccess === false} &&
                         !this.haveErrors(errors) && (
                         <div className="alert alert-danger" role="alert">
-                            Sorry, the form is invalid. Please review, adjust and try again.
+            Sorry, the form is invalid. Please review, adjust and try again.
                         </div>
                     )}
                 </div>
-            </form>
-        );
-    }
+      </form>
+    );
+  }
 } 
