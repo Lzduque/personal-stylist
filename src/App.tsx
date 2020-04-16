fccba021fd9360acb8a5ab3680a35318575a9354
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-16 14:52:40 -0400

    add footer with contact info

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,15 @@ const App = () => {
           < CapsuleForm />
         </div>
       </section>
+      <footer className="bg-black-20 tc fw4 pa2 mt3 white">
+        <h4 className="ma1">Do you like it? Contact Me:</h4>
+        <ul className="icons list mb1 pl1">
+          <li><i className="fas fa-phone-alt fa-1x pb1"></i>   647.862.8984</li>
+          <li><i className="fas fa-paper-plane fa-1x pb1"></i>   lzduque@hotmail.com</li>
+          <li><a href="https://www.linkedin.com/in/leticia-zamolo-duque-0266a233/"><i className="fab fa-linkedin-in fa-2x ph2 white " ></i></a>
+            <a href="https://github.com/Lzduque"><i className="fab fa-github-alt fa-2x ph2 white " ></i></a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
