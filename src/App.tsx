f1877070a2605ae6694b18de5960466850d3d00f
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-20 16:27:17 -0400

    header styling and credits

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,19 @@ const App = () => {
           < CapsuleForm />
         </div>
       </section>
-      <footer className="bg-black-20 tc fw4 pa2 mt3 white">
-        <h4 className="ma1">Do you like it? Contact Me:</h4>
-        <ul className="icons list mb1 pl1">
-          <li><i className="fas fa-phone-alt fa-1x pb1"></i>   647.862.8984</li>
-          <li><i className="fas fa-paper-plane fa-1x pb1"></i>   lzduque@hotmail.com</li>
-          <li><a href="https://www.linkedin.com/in/leticia-zamolo-duque-0266a233/"><i className="fab fa-linkedin-in fa-2x ph2 white " ></i></a>
-            <a href="https://github.com/Lzduque"><i className="fab fa-github-alt fa-2x ph2 white " ></i></a></li>
-        </ul>
+      <footer className="bg-black-20 fw4 pa3 mt3 white flex justify-between">
+        <div className="tl w-25">
+          <h4 className="ma1">Do you like it? Contact Me:</h4>
+          <ul className="icons list mb1 pl1">
+            <li className="pb1" ><a href="https://github.com/Lzduque"><i className="fab fa-github-alt fa-1x white ph2" ></i></a>  Lzduque</li>
+            <li className="pb1" ><i className="fas fa-phone-alt fa-1x ph2"></i>   647.862.8984</li>
+            <li className="pb1" ><a href="https://www.linkedin.com/in/leticia-zamolo-duque-0266a233/"><i className="fab fa-linkedin fa-1x white ph2" ></i></a>  Leticia Zamolo Duque</li>
+            <li ><i className="fas fa-paper-plane fa-1x ph2"></i>   lzduque@hotmail.com</li>
+          </ul>
+        </div>
+        <div className="tr w-25 f6 self-end">
+          Icon made by <a className="white " href="https://www.flaticon.com/authors/freepik" >Freepik</a> from <a className="white"href="https://www.flaticon.com">www.flaticon.com</a>
+        </div>
       </footer>
     </div>
   );
