4c3f2735ab37b7722f0b79ecfc1bc2230eb2b617
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-04-23 19:29:56 -0400

    Update Loader.tsx

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className="flex items-center la-ball-spin-clockwise-fade la-dark flex justify-center center cf fl pa2 mb3">
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
   )
 }
 
