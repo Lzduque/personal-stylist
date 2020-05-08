45eb00370c9a0a65dfd5ffc21981d68d2e034cb6
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-05-08 18:06:40 -0400

    Explain better "#OfOutfits"

--- src/Components/NumberOfOutfitsField.tsx
+++ src/Components/NumberOfOutfitsField.tsx
@@ -21,7 +21,8 @@ const NumberOfOutfitsField = ({ selectedNumberOfOutfits, updateField}: IProps) =
     <div className="numberOfOutfits mt0 mb3-ns">
       <h3 className="mt0" >Number of Outfits</h3>
       <p className="fw4 tl">
-        To choose the number of outfits you want in your capsule wardrobe, you should consider your lifestyle, closet size and laundry habits.
+        To choose the number of outfits you want in your capsule wardrobe, you should consider your lifestyle, closet size, and laundry habits. If you want one different outfit for each day of the summer, you need 90 different outfits.
+        The more outfits you want, the more clothes you will need to make it work. The clothes are combined so each outfit is composed of a unique combination of some of your pieces, for example, 1 pair of pants, 1 top, 1 coat, and one pair of shoes.
       </p>
       <Select
         className="select"
