4cc0b5c9480241e1694131d92a4e313911905399
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-06-11 18:40:05 -0400

    Updates

---
+++ precache-manifest.915cb27d38db37f18e9e2886ad153873.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfefa4fdcf5e338d4562c1cdf39d3266",
+    "url": "/personal-stylist/index.html"
+  },
+  {
+    "revision": "7e4a9e84bc794ff1536d",
+    "url": "/personal-stylist/static/css/main.ef656b31.chunk.css"
+  },
+  {
+    "revision": "bc2349715f6507fda740",
+    "url": "/personal-stylist/static/js/2.d45f9e31.chunk.js"
+  },
+  {
+    "revision": "3df497401d324ee6ed76d4d98715bc5f",
+    "url": "/personal-stylist/static/js/2.d45f9e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4a9e84bc794ff1536d",
+    "url": "/personal-stylist/static/js/main.0359462e.chunk.js"
+  },
+  {
+    "revision": "fe03f2ab09cfe32613c4",
+    "url": "/personal-stylist/static/js/runtime-main.063a0d80.js"
+  },
+  {
+    "revision": "8b237fc1a1d9654441003cf18123b630",
+    "url": "/personal-stylist/static/media/copy.8b237fc1.svg"
+  },
+  {
+    "revision": "cdb413ff583a81c3e8f1c88bc58775bc",
+    "url": "/personal-stylist/static/media/facebook.cdb413ff.svg"
+  },
+  {
+    "revision": "4b760778d8f03a4b47478070b00a5d6c",
+    "url": "/personal-stylist/static/media/mail.4b760778.svg"
+  },
+  {
+    "revision": "0aa499368fc5fe006bd574f50619eb7d",
+    "url": "/personal-stylist/static/media/whatsapp.0aa49936.svg"
+  }
+]);
